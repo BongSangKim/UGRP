dcecb62997bd54f2ebc73bbf82f3650b069153be
BongSangKim <47958731+BongSangKim@users.noreply.github.com>
2019-07-11 19:16:04 +0900

    7/11 Env 구현, RL code 구현중..
    ㅜㅜ

--- RL+Env = RLEnv/RL이랑 Env 합쳐보기.py
+++ RL+Env = RLEnv/RL이랑 Env 합쳐보기.py
@@ -1,19 +1,14 @@
 from __future__ import division  #python 2.x 호환. 없어도 상관없음
-import time        #time.time() 사용, 현재시간 호출
-import math        #제곱근, log 사용
-import random      #random.choice()사용
-import gym
+import time, math, random, gym
 #from gym import space, logger
 #from gym.utils import seeding
 import numpy as np
 import tensorflow as tf
 #import pandas as pd
 
+#__dict__로 변수 내부 확인가능, 디버그할때 참고
 #=====================Environment code=========================================
-#US_Xposition,Yposition ->UE_Xposition,Yposition으로?
-#assosiation 부분 association으로 변경함(오타수정)
 class UDNEnv(gym.Env):
-
 	def __init__(self):  #변수 선언
 		self.d_attenuation = -4 #거리감쇠 alpha값(NLoS)
 		self.BSposition = np.transpose(np.loadtxt('BSposition.csv', delimiter=','))  #BS위치는 변하지 않으므로 ppp로 뿌려진 좌표값(csv파일, 1 by n일듯) 사용, ppp.py로 만들고 경로 일치시키기(pandas 사용)
@@ -31,11 +26,12 @@ class UDNEnv(gym.Env):
 		self.BSdistance = tf.zeros([self.BSnum,self.usernum]) 
 		self.user_association = tf.zeros([1,self.usernum])  
 		self.SNR = None
+		self.timeLimit = 10000 #mcts 라이브러리에서 UDNEnv 클래스로 이동
 
 	def step(self, action):
 		state = action
 		self.Econsumption = tf.reduce_sum(state)   #state가 1차원 벡터니까 reduce_sum은 스칼라. Econsumption은 total값임
-		
+
 		#BS-User 거리 계산
 		for i in range(self.BSnum):
 			for j in range(self.usernum):
@@ -44,7 +40,7 @@ class UDNEnv(gym.Env):
 		self.BSdistance = np.linalg.norm(self.BS_user_distance, axis=2, ord = 2)
 		self.distance = np.array(self.BSdistance)    #np.array랑  tf행렬이랑 계산되나?? tf쪽으로 계산되는듯
 		#거리 계산 종료
-		
+
 		self.user_association = self.association(self.state, self.BSdistance)
 		self.SNR = tf.pow(self.user_association,self.d_at_tensor) #SNR 계산
 
@@ -63,7 +59,6 @@ class UDNEnv(gym.Env):
 			if state[i] == 0:
 				for j in len(self.usernum):
 					associationdistance[i][j] = math.inf  #나중에 계산 오류날 수도 있으니까 inf값 대신 area밖으로 나가는 특정값을 쓰는 것 고려.
-		
 		association_distance = tf.math.reduce_min(associationdistance, axis = 0)
 		return association_distance
 
@@ -71,18 +66,19 @@ class UDNEnv(gym.Env):
 		self.state = tf.ones([1,self.BSnum])
 		self.done = False
 		return self.state
-###추가코드
-	def isTerminal(): #return은 boolean으로 
-		if :#state is terminal
+	###추가코드
+	def isTerminal(self): #일단 시간조건
+		if time.time() > self.timeLimit: #state is terminal 
 			return True
 		else: #state is nonterminal
 			return False
-	def getPossibleActions(UDNEnv.state):
+	def getPossibleActions(self):
 		pass
-	def takeAction(action):
+	def takeAction(action): #action=mcts.search(initialstate=Env.state)
 		pass
-	def getReward(UDNEnv.state):
+	def getReward(self):
 		pass
+Env = UDNEnv() #Env로 인스턴스 호출
 #=====================Environment code=========================================
 
 #############################RL code-MCTS######################################
@@ -102,8 +98,6 @@ class state():
 ##############################original code####################################
 '''
 #######state class 제거 및 함수변수로 UDNEnv.state 사용#########################
-Env = UDNEnv()
-initialState = Env.state
 
 
 
@@ -112,107 +106,112 @@ initialState = Env.state
 ###############################################################################    
 
 def randomPolicy(state):
-    while not state.isTerminal():   #state.isTerminal() 등 함수 4개는 Env.isTerminal()형태로 
-        try:
-            action = random.choice(state.getPossibleActions())  #random.choice('아마 iterable변수')=하나 random으로 골라 return해줌
-        except IndexError:
-            raise Exception("Non-terminal state has no possible actions: " + str(state))
-        state = state.takeAction(action) #action에 따라 state 업데이트
-    return state.getReward()
+	while not Env.isTerminal():   #state.isTerminal() 등 함수 4개는 Env.isTerminal()형태로 
+		try:
+			action = random.choice(state.getPossibleActions())  #random.choice('아마 iterable변수')=하나 random으로 골라 return해줌
+		except IndexError:
+			raise Exception("Non-terminal state has no possible actions: " + str(state))
+		state = state.takeAction(action) #action에 따라 state 업데이트
+	return state.getReward()
 
 
 class treeNode():                               #트리 노드 정의. 노드에 state 정해주면, state.isTerminal()값에 따라 노드가 터미널노드인지 결정됨
-    def __init__(self, state, parent):
-        self.state = state
-        self.isTerminal = state.isTerminal()
-        self.isFullyExpanded = self.isTerminal
-        self.parent = parent
-        self.numVisits = 0
-        self.totalReward = 0
-        self.children = {}
+	def __init__(self, state, parent):
+		self.state = state #Env.state
+		self.isTerminal = Env.isTerminal()   
+		self.isFullyExpanded = self.isTerminal
+		self.parent = parent
+		self.numVisits = 0
+		self.totalReward = 0
+		self.children = {}
+
+
 
 class mcts():                   #explorationConstant는 값을 바꾸어 학습시킬 수 있다. 
-    def __init__(self, timeLimit=None, iterationLimit=None, explorationConstant=1 / math.sqrt(2), rolloutPolicy=randomPolicy):
-        #timeLimit,iterationLimit는 입력안하면 None이 초기값, 둘다 입력안하면 바로 아래 ValueError나옴
-        #rolloutPolicy는 따로 입력해주지 않으면 randompolicy인데, 나중에 UCB1값을 비교하는 식으로 하는 것이 좋을듯.
-        #처음에는 라이브러리 기본대로 randompolicy함수 만들어서 하고, 이후 UCB1 함수 따로 만들어서 rolloutPolicy 변수값을 UCB1으로 넣어보기
-        if timeLimit != None:
-            if iterationLimit != None:
-                raise ValueError("Cannot have both a time limit and an iteration limit")  #
-            # time taken for each MCTS search in milliseconds
-            self.timeLimit = timeLimit
-            self.limitType = 'time'
-        else: #timeLimit = None일때
-            if iterationLimit == None:
-                raise ValueError("Must have either a time limit or an iteration limit")
-            # number of iterations of the search
-            if iterationLimit < 1:
-                raise ValueError("Iteration limit must be greater than one")
-            self.searchLimit = iterationLimit
-            self.limitType = 'iterations'
-        self.explorationConstant = explorationConstant
-        self.rollout = rolloutPolicy
+	def __init__(self, timeLimit=None, iterationLimit=None, explorationConstant=1 / math.sqrt(2), rolloutPolicy=randomPolicy):
+		#timeLimit,iterationLimit는 입력안하면 None이 초기값, 둘다 입력안하면 바로 아래 ValueError나옴
+		#rolloutPolicy는 따로 입력해주지 않으면 randompolicy인데, 나중에 UCB1값을 비교하는 식으로 하는 것이 좋을듯.
+		#처음에는 라이브러리 기본대로 randompolicy함수 만들어서 하고, 이후 UCB1 함수 따로 만들어서 rolloutPolicy 변수값을 UCB1으로 넣어보기
+		if timeLimit != None:
+			if iterationLimit != None:
+				raise ValueError("Cannot have both a time limit and an iteration limit")  #
+			# time taken for each MCTS search in milliseconds
+			self.timeLimit = timeLimit
+			self.limitType = 'time'
+		else: #timeLimit = None일때
+			if iterationLimit == None:
+				raise ValueError("Must have either a time limit or an iteration limit")
+			# number of iterations of the search
+			if iterationLimit < 1:
+				raise ValueError("Iteration limit must be greater than one")
+			self.searchLimit = iterationLimit
+			self.limitType = 'iterations'
+		self.explorationConstant = explorationConstant
+		self.rollout = rolloutPolicy
+
+	def search(self, initialState):  #initialstate=Env.state
+		self.root = treeNode(initialState, None) #라이브러리 앞에 있는 treenode class
+		print(self.limitType)
+		if self.limitType == 'time':  #limitType이 time일때
+			timeLimit = time.time() + self.timeLimit / 1000
+			while time.time() < timeLimit: #timeLimit전까지 계속 search.....
+				self.executeRound()
+		else: #limitType 이 iterations일때
+			for i in range(self.searchLimit):
+				self.executeRound()  #SearchLmit전까지 계속 search...
+
+		bestChild = self.getBestChild(self.root, 0)
+		return self.getAction(self.root, bestChild)
+
+	def executeRound(self):
+		node = self.selectNode(self.root)
+		reward = self.rollout(node.state)
+		self.backpropogate(node, reward)
+
+	def selectNode(self, node):
+		while not node.isTerminal:
+			if node.isFullyExpanded:
+				node = self.getBestChild(node, self.explorationConstant)
+			else:
+				return self.expand(node)
+		return node
+
+	def expand(self, node):
+		actions = node.state.getPossibleActions()
+		for action in actions:
+			if action not in node.children:
+				newNode = treeNode(node.state.takeAction(action), node)
+				node.children[action] = newNode
+				if len(actions) == len(node.children):
+					node.isFullyExpanded = True
+				return newNode
+
+		raise Exception("Should never reach here")
+
+	def backpropogate(self, node, reward):
+		while node is not None:
+			node.numVisits += 1
+			node.totalReward += reward
+			node = node.parent
+
+	def getBestChild(self, node, explorationValue):
+		bestValue = float("-inf")
+		bestNodes = []
+		for child in node.children.values():
+			nodeValue = child.totalReward / child.numVisits + explorationValue * math.sqrt(2 * math.log(node.numVisits) / child.numVisits)
+			if nodeValue > bestValue:
+				bestValue = nodeValue
+				bestNodes = [child]
+			elif nodeValue == bestValue:
+				bestNodes.append(child)
+		return random.choice(bestNodes)
+
+	def getAction(self, root, bestChild):
+		for action, node in root.children.items():
+			if node is bestChild:
+				return action
 
 
-    def search(self, initialState):             #라이브러리 실행 이전에 initialState=UDNEnv.state 코드 한줄 넣기
-        self.root = treeNode(initialState, None) #라이브러리 앞에 있는 treenode class
-
-        if self.limitType == 'time':  #limitType이 time일때
-            timeLimit = time.time() + self.timeLimit / 1000
-            while time.time() < timeLimit: #timeLimit전까지 계속 search.....
-                self.executeRound()
-        else: #limitType 이 iterations일때
-            for i in range(self.searchLimit):
-                self.executeRound()  #SearchLmit전까지 계속 search...
-
-        bestChild = self.getBestChild(self.root, 0)
-        return self.getAction(self.root, bestChild)
-
-    def executeRound(self):
-        node = self.selectNode(self.root)
-        reward = self.rollout(node.state)
-        self.backpropogate(node, reward)
-
-    def selectNode(self, node):
-        while not node.isTerminal:
-            if node.isFullyExpanded:
-                node = self.getBestChild(node, self.explorationConstant)
-            else:
-                return self.expand(node)
-        return node
-
-    def expand(self, node):
-        actions = node.state.getPossibleActions()
-        for action in actions:
-            if action not in node.children:
-                newNode = treeNode(node.state.takeAction(action), node)
-                node.children[action] = newNode
-                if len(actions) == len(node.children):
-                    node.isFullyExpanded = True
-                return newNode
-
-        raise Exception("Should never reach here")
-
-    def backpropogate(self, node, reward):
-        while node is not None:
-            node.numVisits += 1
-            node.totalReward += reward
-            node = node.parent
-
-    def getBestChild(self, node, explorationValue):
-        bestValue = float("-inf")
-        bestNodes = []
-        for child in node.children.values():
-            nodeValue = child.totalReward / child.numVisits + explorationValue * math.sqrt(
-                2 * math.log(node.numVisits) / child.numVisits)
-            if nodeValue > bestValue:
-                bestValue = nodeValue
-                bestNodes = [child]
-            elif nodeValue == bestValue:
-                bestNodes.append(child)
-        return random.choice(bestNodes)
-
-    def getAction(self, root, bestChild):
-        for action, node in root.children.items():
-            if node is bestChild:
-                return action
+test=Env.state
+initialState = Env.state
+action = mcts.search(Env,initialState=initialState)
